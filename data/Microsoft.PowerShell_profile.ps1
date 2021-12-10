99df145c4d521d5a66e914811b47a6f35d9280f4
dsn <dsn@dsnn.io>
2021-12-10 09:52:44 +0100

    cleanup

--- data/Microsoft.PowerShell_profile.ps1
+++ data/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,34 @@
-Import-Module posh-git
-Import-Module prompt
+if (-not (Get-Module -ListAvailable -Name PowerShellGet)) {
+    Write-Host "PowerShellGet not found, installing..."
+    Install-Module -Name PowerShellGet -Force
+}
+
+if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
+    Write-Host "PSReadLine not found, installing..."
+    Install-Module PSReadLine -AllowPrerelease -Force
+}
+
+if (-not (Get-Module -ListAvailable -Name posh-git)) {
+    Write-Host "posh-git not found, installing..."
+    PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+}
+
+if (-not (Get-Module -ListAvailable -Name z)) {
+    Write-Host "z not found, installing..."
+    Install-Module -Name z 
+}
+
+if (Get-Module -ListAvailable -Name PSReadLine) {
+    Import-Module PSReadLine
+} 
+
+if (Get-Module -ListAvailable -Name posh-git) {
+    Import-Module posh-git
+} 
+
+if (Get-Module -ListAvailable -Name prompt) {
+    Import-Module prompt
+} 
 
 if (Get-Module -ListAvailable -Name work) {
     Import-Module work
@@ -8,6 +37,15 @@ if (Get-Module -ListAvailable -Name work) {
 if (Get-Module -ListAvailable -Name home) {
     Import-Module home
 } 
+
+# General aliases
+function which() { & get-command $args }
+
+# Navigation aliases
+
+function l() { ls }
+function ..() { cd ..}
+function ...() { cd ../..}
 
 # Dotnet aliases
 function dnn () { dotnet run --no-build }
@@ -23,48 +61,70 @@ function nt () { npm test }
 function ntu () { npm run test:update-snapshot }
 
 # Edit configs
-function cfc () { code $profile.CurrentUserAllHosts }
+function cfc () { code $profile }
 function cfh () { code "C:\Windows\System32\drivers\etc\hosts" }
 function cfcw () { code $PSScriptRoot\Modules\work\work.psm1 }
 
 # Git aliases
-function Get-GitStatus { & git st }
-New-Alias -Name gs -Value Get-GitStatus -Force -Option AllScope
-
-function Get-GitLog { & git ls }
-New-Alias -Name gl -Value Get-GitLog -Force -Option AllScope
-
-function Get-GitCommit { & git commit -m $args }
-New-Alias -Name gc -Value Get-GitCommit -Force -Option AllScope
-
-function Get-GitAdd { & git add $args }
-New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
-
-function Get-GitPush { & git push }
-New-Alias -Name gp -Value Get-GitPush -Force -Option AllScope
-
-function Get-GitPushForce { & git push --force-with-lease }
-New-Alias -Name gpf -Value Get-GitPushForce -Force -Option AllScope
-
-function Get-GitPullRebase { & git pr }
-New-Alias -Name pr -Value Get-GitPullRebase -Force -Option AllScope
-
-function Get-GitFetch { & git fetch $args }
-New-Alias -Name gf -Value Get-GitFetch -Force -Option AllScope
-
-function Get-GitCheckout { & git checkout $args }
-New-Alias -Name co -Value Get-GitCheckout -Force -Option AllScope
-
-function Get-ListLocalBranches { & git bl }
-New-Alias -Name bl -Value Get-ListLocalBranches -Force -Option AllScope
-
-function Get-ListRemoteBranches { & git br }
-New-Alias -Name bl -Value Get-ListRemoteBranches -Force -Option AllScope
-
-function Get-ListRemoteBranches { & git rev-parse --abbrev-ref HEAD }
-New-Alias -Name cb -Value Get-ListRemoteBranches -Force -Option AllScope
+function gs() { git status}
+function gll() { git log } # ffs ms. change to 'gl' later. requires to be forcefully deleted
+function gc() { & git commit -am $args }
+function ga() { & git add $args }
+function gp() { git push }
+function gpf() { git push --force-with-lease }
+function pr() { git pull --rebase }
+function gf() { git fetch }
+function co() { & git checkout $args }
+function bl() { git branch -l }
+function br() { git branch -r }
+function cb() { git rev-parse --abbrev-ref HEAD }
 
 # Source profile
 function rf() {
-    . $PROFILE.CurrentUserAllHosts
+    . $PROFILE
 }
+
+function ff() {
+    $fzf_path = "C:\stuff\bin\fzf.exe"
+    if (-not (Test-Path -Path $fzf_path -PathType Leaf)) {
+        Write-Host "FZF binary not found"
+    } elseif ((Get-Command "fzf" -ErrorAction SilentlyContinue) -eq $null) {
+        Write-Host "fzf not in your PATH"
+    } else {
+        $result = git branch | fzf | Out-String 
+        Invoke-Expression "& git checkout $result"
+    }
+    
+}
+
+# TODO: review this later (I need to sleep now) 
+# function Load-Module ($m) {
+
+#     # If module is imported say that and do nothing
+#     if (Get-Module | Where-Object {$_.Name -eq $m}) {
+#         write-host "Module $m is already imported."
+#     }
+#     else {
+
+#         # If module is not imported, but available on disk then import
+#         if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m}) {
+#             Import-Module $m -Verbose
+#         }
+#         else {
+
+#             # If module is not imported, not available on disk, but is in online gallery then install and import
+#             if (Find-Module -Name $m | Where-Object {$_.Name -eq $m}) {
+#                 Install-Module -Name $m -Force -Verbose -Scope CurrentUser
+#                 Import-Module $m -Verbose
+#             }
+#             else {
+
+#                 # If module is not imported, not available and not in online gallery then abort
+#                 write-host "Module $m not imported, not available and not in online gallery, exiting."
+#                 EXIT 1
+#             }
+#         }
+#     }
+# }
+
+# Load-Module "ModuleName" # Use "PoshRSJob" to test it out
